1acb4036915542cdd2fe838add943a5d70a6f71e
Nancy Fan <hmfnj1996@gmail.com>
2016-12-29 19:56:25 +0800

    4.0 commit: Wrong Blending without SIMD?

--- nosimd.cpp
+++ nosimd.cpp
@@ -33,28 +33,30 @@ void blending_without_simd(RGB & rgb1, RGB & rgb2, int A, bool mode){
     if(mode){
         int _A = 256 - A;
         for(int iY = 0; iY < rgb1.size; iY++){
-            rgb1.pR16[iY] = (A * rgb1.pR16[iY] + _A * rgb2.pR16[iY]) /256;
-            rgb1.pG16[iY] = (A * rgb1.pG16[iY] + _A * rgb2.pG16[iY]) /256;
-            rgb1.pB16[iY] = (A * rgb1.pB16[iY] + _A * rgb2.pB16[iY]) /256;
+            rgb1.pR16[iY] = (A * rgb1.pR16[iY] + _A * rgb2.pR16[iY]) >> 8;
+            rgb1.pG16[iY] = (A * rgb1.pG16[iY] + _A * rgb2.pG16[iY]) >> 8;
+            rgb1.pB16[iY] = (A * rgb1.pB16[iY] + _A * rgb2.pB16[iY]) >> 8;
         }
     }
     else{
         for(int iY = 0; iY < rgb1.size; iY++){
-            rgb1.pR16[iY] = (A * rgb1.pR16[iY]) /256;
-            rgb1.pG16[iY] = (A * rgb1.pG16[iY]) /256;
-            rgb1.pB16[iY] = (A * rgb1.pB16[iY]) /256;
+            rgb1.pR16[iY] = ((uint16_t)A * rgb1.pR16[iY]) >> 8;
+            rgb1.pG16[iY] = ((uint16_t)A * rgb1.pG16[iY]) >> 8;
+            rgb1.pB16[iY] = ((uint16_t)A * rgb1.pB16[iY]) >> 8;
         }
     }
 }
 
 void rgb2yuv_without_simd(YUV & yuv, RGB & rgb){
-    for(int iY = 0; iY < yuv.size; iY++){
-        yuv.pY16[iY] = 0.299*rgb.pR16[iY] + 0.587*rgb.pG16[iY] + 0.114*rgb.pB16[iY];
-        
-        if(((iY/yuv.width)&1)&&((iY%yuv.width)&1)){
-            int iUV = yuv.getBlockID(iY);
-            yuv.pU16[iUV] = -0.147*rgb.pR16[iY] - 0.289*rgb.pG16[iY] + 0.436*rgb.pB16[iY] + 128;
-            yuv.pV16[iUV] = 0.615*rgb.pR16[iY] - 0.515*rgb.pG16[iY] - 0.100*rgb.pB16[iY] + 128;
+    for(int row = 0, iY = 0, iUV = 0; row < yuv.height; row++){
+        for(int col = 0; col < yuv.width; col++, iY++){
+            yuv.pY16[iY] = 0.299*rgb.pR16[iY] + 0.587*rgb.pG16[iY] + 0.114*rgb.pB16[iY];
+            
+            if((row&1)&&(col&1)){
+                yuv.pU16[iUV] = -0.147*rgb.pR16[iY] - 0.289*rgb.pG16[iY] + 0.436*rgb.pB16[iY] + 128;
+                yuv.pV16[iUV] = 0.615*rgb.pR16[iY] - 0.515*rgb.pG16[iY] - 0.100*rgb.pB16[iY] + 128;
+                iUV++;
+            }
         }
     }// get one picture
 
@@ -69,11 +71,14 @@ int process_without_simd(YUV &OUT_YUV, YUV &DEM1_YUV, YUV &DEM2_YUV, RGB &CHECK_
 
     yuv2rgb_without_simd(DEM1_YUV, CHECK_RGB1);
     if(mode)    yuv2rgb_without_simd(DEM2_YUV, CHECK_RGB2);
-    
 
+    CHECK_RGB1.write(foutcheck1);
+    
     /*************** Blending ****************/
     for(int A = 1; A < 256; A += 3){
         blending_without_simd(CHECK_RGB1, CHECK_RGB2, A, mode);
+        if(A == 253)
+            CHECK_RGB1.write(foutcheck2);
         rgb2yuv_without_simd(OUT_YUV, CHECK_RGB1);
         
         total_time += clock() - core_time;
@@ -90,11 +95,11 @@ int process_without_simd(YUV &OUT_YUV, YUV &DEM1_YUV, YUV &DEM2_YUV, RGB &CHECK_
     if(mode){
         cout << "Alpha Blending with dem1.yuv and dem2.yuv, output file is \"alpha0-2.yuv\":" <<endl;
         cout << "Core function time: " << (double)total_time / CLOCKS_PER_SEC * 1000<< "ms" << endl;
-        cout << "Total run time: " << (double)(clock() - begin_time) / CLOCKS_PER_SEC * 1000 << "ms" << endl << endl;
+        cout << "Include output time: " << (double)(clock() - begin_time) / CLOCKS_PER_SEC * 1000 << "ms" << endl << endl;
     }else{
         cout << "Alpha Blending with dem1.yuv, output file is \"alpha0-1.yuv\":" <<endl;
         cout << "Core function time: " << (double)total_time / CLOCKS_PER_SEC * 1000<< "ms" << endl;
-        cout << "Total run time: " << (double)(clock() - begin_time) / CLOCKS_PER_SEC * 1000 << "ms" << endl << endl;
+        cout << "Include output time: " << (double)(clock() - begin_time) / CLOCKS_PER_SEC * 1000 << "ms" << endl << endl;
     }
     
     
